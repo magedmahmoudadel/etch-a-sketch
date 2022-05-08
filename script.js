e5417b0593196575e963882d58a840caee3bd2b6
Maged Mahmoud Adel <magedmahmoud94@outlook.com>
2022-05-08 18:33:18 +0200

    Create etch-a-sketch functionality and reset button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('.container');
-
+const resetBtn = document.querySelector('.reset');
+const gridBtn = document.querySelector('.grid');
 function createGrid(num){
+    //Takes in a number and creates a grid of squares with dimensions num*num
     const gridDimensions = num*num;
     let divDimensions= num/gridDimensions*100;
     for(let i = 0; i<gridDimensions; i++){
@@ -13,6 +15,29 @@ function createGrid(num){
     }
 }
 
-createGrid(2);
+function etchASketch(){
+    const divs = document.querySelectorAll('.etch');
+    for(let i = 0; i<divs.length; i++){
+        divs[i].addEventListener('mouseover',()=>{
+            divs[i].style.backgroundColor='black';
+        })
+    }
+}
+
+function resetGrid(){
+    const divs = document.querySelectorAll('.etch');
+    for (let i = 0; i<divs.length; i++){
+        divs[i].style.backgroundColor='white';
+    }
+}
+
+
+resetBtn.addEventListener('click',()=>{
+    resetGrid();
+})
+
+createGrid(16);
+etchASketch();
+
 
 
